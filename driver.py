2b553ec94c20dce15be349dcd66bb458bc3d6e60
Vishwesh Vhavle <vishweshvhavle@gmail.com>
2021-09-26 03:17:39 +0530

    Added target position finder

--- driver.py
+++ driver.py
@@ -19,9 +19,23 @@ class bot(object):
 
         self.right_enable = 0
         self.left_enable = 0
+        self.error = 0
+
+        self.P = 0
+        self.I = 0
+        self.D = 0
+
+        self.Kp = 0
+        self.Ki = 0
+        self.Kd = 0
 
         self.orientation = 'SOUTH'
+        self.next_orientation = 'SOUTH'
+        self.next = 'NONE'
+        self.target = (0,0)
         self.route_path = []
+
+        self.complete = False
 
 class Node(object):
 
@@ -235,6 +249,7 @@ def get_perspective_image(frame):
         if dist < min_dist:
             min_dist = dist
             X1,Y1 = x,y
+
     min_dist = 1000000
     for i in raw_points:
         x,y = i
@@ -242,6 +257,7 @@ def get_perspective_image(frame):
         if dist < min_dist:
             min_dist = dist
             X2,Y2 = x,y
+
     min_dist = 1000000
     for i in raw_points:
         x,y = i
@@ -249,6 +265,7 @@ def get_perspective_image(frame):
         if dist < min_dist:
             min_dist = dist
             X3,Y3 = x,y
+
     min_dist = 1000000
     for i in raw_points:
         x,y = i
@@ -273,115 +290,206 @@ def get_distance(x1,y1,x2,y2):
     distance = math.hypot(x2 - x1, y2 - y1)
     return distance
 
+def get_position(virtual):
+    return real
 
-def communicate(bot):
-        for i in range(len(bot.route_path)):
-            time.sleep(1)
-            if bot.route_path[i] == (0,0):
-                pay()
+def target(bot):
+    for i in range(len(bot.route_path)):
+        if bot.route_path[i] == (0,0):
+            bot.next = 'PAY'
+            bot.route_path = bot.route_path[i+1:]
+            break
 
-            elif i+1 < len(bot.route_path):
-                if (bot.route_path[i+1][0] - bot.route_path[i][0], bot.route_path[i+1][1] - bot.route_path[i][1]) == (0,1):
-                    if orientation == 'SOUTH':
-                        forward()
+        elif i+1 == len(bot.route_path):
+            bot.route_path = []
+            bot.next = 'END'
 
-                    elif orientation == 'NORTH':
-                        back()
-                        forward()
+        elif i+1 < len(bot.route_path):
+            x2 = bot.route_path[i+1][0]
+            x1 = bot.route_path[i][0]
+            y2 = bot.route_path[i+1][1]
+            y1 = bot.route_path[i][1]
+            if (x2-x1, y2-y1) == (0,1):
+                if bot.orientation == 'SOUTH':
+                    bot.target = (x2,y2)
+                    continue
 
-                    elif orientation == 'WEST':
-                        left()
-                        forward()
+                elif bot.orientation == 'NORTH':
+                    bot.next = 'BACK'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'SOUTH'
+                    break
 
-                    elif orientation == 'EAST':
-                        right()
-                        forward()
+                elif bot.orientation == 'WEST':
+                    bot.next = 'LEFT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'SOUTH'
+                    break
 
-                    orientation = 'SOUTH'
+                elif bot.orientation == 'EAST':
+                    bot.next = 'RIGHT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'SOUTH'
+                    break
 
-                elif (bot.route_path[i+1][0] - bot.route_path[i][0], bot.route_path[i+1][1] - bot.route_path[i][1]) == (0,-1):
-                    if orientation == 'NORTH':
-                        forward()
+            elif (x2-x1, y2-y1) == (0,-1):
+                if bot.orientation == 'NORTH':
+                    bot.target = (x2,y2)
+                    continue
 
-                    elif orientation == 'SOUTH':
-                        back()
-                        forward()
+                elif bot.orientation == 'SOUTH':
+                    bot.next = 'BACK'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'NORTH'
+                    break
 
-                    elif orientation == 'WEST':
-                        right()
-                        forward()
+                elif bot.orientation == 'WEST':
+                    bot.next = 'RIGHT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'NORTH'
+                    break
 
-                    elif orientation == 'EAST':
-                        left()
-                        forward()
+                elif bot.orientation == 'EAST':
+                    bot.next = 'LEFT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'NORTH'
+                    break
 
-                    orientation = 'NORTH'
+            elif (x2-x1, y2-y1) == (1,0):
+                if bot.orientation == 'EAST':
+                    bot.target = (x2,y2)
+                    continue
 
-                elif (bot.route_path[i+1][0] - bot.route_path[i][0], bot.route_path[i+1][1] - bot.route_path[i][1]) == (1,0):
-                    if orientation == 'EAST':
-                        forward()
+                elif bot.orientation == 'SOUTH':
+                    bot.next = 'LEFT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'EAST'
+                    break
 
-                    elif orientation == 'SOUTH':
-                        left()
-                        forward()
+                elif bot.orientation == 'WEST':
+                    bot.next = 'BACK'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'EAST'
+                    break
 
-                    elif orientation == 'WEST':
-                        back()
-                        forward()
+                elif bot.orientation == 'NORTH':
+                    bot.next = 'RIGHT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'EAST'
+                    break
 
-                    elif orientation == 'NORTH':
-                        right()
-                        forward()
+            elif (x2-x1, y2-y1) == (-1,0):
+                if bot.orientation == 'WEST':
+                    bot.target = (x2,y2)
+                    continue
 
-                    orientation = 'EAST'
+                elif bot.orientation == 'SOUTH':
+                    bot.next = 'RIGHT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'WEST'
+                    break
 
-                elif (bot.route_path[i+1][0] - bot.route_path[i][0], bot.route_path[i+1][1] - bot.route_path[i][1]) == (-1,0):
-                    if orientation == 'WEST':
-                        forward()
+                elif bot.orientation == 'NORTH':
+                    bot.next = 'LEFT'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'WEST'
+                    break
 
-                    elif orientation == 'SOUTH':
-                        right()
-                        forward()
+                elif bot.orientation == 'EAST':
+                    bot.next = 'BACK'
+                    bot.route_path = bot.route_path[i:]
+                    bot.next_orientation = 'WEST'
+                    break
+    return bot
 
-                    elif orientation == 'NORTH':
-                        left()
-                        forward()
-
-                    elif orientation == 'EAST':
-                        back()
-                        forward()
-
-                    orientation = 'WEST'
-
-def forward():
+def right(bot):
     driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
-    driver.get("http://192.168.11.6/forward1")
+    driver.get("http://192.168.11.6/right")
     driver.close()
 
-def back():
+def left(bot):
     driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
-    driver.get("http://192.168.11.6/back1")
+    driver.get("http://192.168.11.6/left")
     driver.close()
 
-def right():
+def back(bot):
     driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
-    driver.get("http://192.168.11.6/right1")
+    driver.get("http://192.168.11.6/back")
     driver.close()
 
-def left():
+def pay(bot):
     driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
-    driver.get("http://192.168.11.6/left1")
+    driver.get("http://192.168.11.6/pay")
     driver.close()
 
-def pay():
-    driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
-    driver.get("http://192.168.11.6/pay1")
-    driver.close()
+def wrap_angle(angle):
+    # Convert an angle to 0 to 2*pi range.
+    new_angle = np.arctan2(np.sin(angle), np.cos(angle))
+    if new_angle < 0:
+        new_angle = abs(new_angle) + 2 * (np.pi - abs(new_angle))
+    return new_angle
 
-def pid():
-    pass
+def final_path(bot, img):
 
-def main():
+    grn = (0,255,0)
+    red = (0,0,255)
+    blk = (0,0,0)
+    blu = (255,0,0)
+
+    img2[0:400,362:412] = blk
+    img2[350:400,0:406] = blk
+    img2[0:400,606:656] = blk
+    img2[350:400,606:] = blk
+
+    img2[0:50,412:462] = red
+    img2[400:450,56:106] = grn
+
+    cv2.imwrite('/home/vishwesh/Desktop/Geekbot/mid1.jpg',img)
+
+    start_x,start_y,end_x,end_y = get_start_end(img)
+    bot.route_path.append((end_y+1, end_x+1))
+
+    self = path_algorithm()
+    self.grid_map(start_x, start_y, end_x, end_y)
+    self.path_detect(bot.route_path)
+
+    bot.route_path.append((start_y+1, start_x+1))
+
+    route_path_copy = bot.route_path
+    bot.route_path = list(reversed(bot.route_path))
+
+    draw_path(img, bot, end_x+1, end_y+1, start_x+1, start_y+1)
+
+    bot.route_path.append((0,0))
+    bot.route_path += route_path_copy[0:]
+
+    print('Route Path {}:'.format(bot.id), bot.route_path)
+    print()
+    return bot
+
+def pid(bot, current_position):
+    if bot.orientation == 'SOUTH':
+        error = current_position[0] - bot.position[0]
+    elif bot.orientation == 'NORTH':
+        error = bot.position[0] - current_position[0]
+    elif bot.orientation == 'WEST':
+        error = bot.position[1] - current_position[1]
+    elif bot.orientation == 'EAST':
+        error = current_position[1] - bot.position[1]
+
+    bot.P = error
+    bot.I += error
+    bot.D = error - bot.error
+    pid = (bot.Kp*bot.P) + (bot.Ki*bot.I) + (bot.Kd*bot.D)
+
+    bot.right_enable -= pid
+    bot.left_enable += pid
+    bot.error = error
+
+    return bot
+
+if __name__ == '__main__':
+
     parameters = cv2.aruco.DetectorParameters_create()
     aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
 
@@ -389,66 +497,86 @@ def main():
 
     if (cap.isOpened() == False):
         print("Unable to read camera feed")
+        break
 
     cap.set(3, 1920)
     cap.set(4, 1080)
 
+    bot1 = bot(1, position, rotation)
+    bot2 = bot(2, position, rotation)
+    bot3 = bot(3, position, rotation)
+    bot4 = bot(4, position, rotation)
+
     out = cv2.VideoWriter('System Tracking.mp4', cv2.VideoWriter_fourcc('M','J','P','G'), 24, (1920,1080))
 
-    while(True):
+    os.system("nmcli con up GeekNetwork1")
+
+    while (bot1.complete == False):
         ret, frame = cap.read()
 
         if ret == True:
 
-        img = frame.copy()
+            img = get_perspective_image(frame)
 
-        corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
+            corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, aruco_dict, parameters=parameters)
 
-        int_corners = np.int0(corners)
-        cv2.polylines(img, int_corners, True, (0, 255, 0), 2)
+            int_corners = np.int0(corners)
+            cv2.polylines(img, int_corners, True, (0, 255, 0), 2)
 
-        def wrap_angle(angle):
-            # Convert an angle to 0 to 2*pi range.
-            new_angle = np.arctan2(np.sin(angle), np.cos(angle))
-            if new_angle < 0:
-                new_angle = abs(new_angle) + 2 * (np.pi - abs(new_angle))
-            return new_angle
+            markers = []
 
-        markers = []
-        # red, blue, yellow, purple (TL, TR, BR, BL)
-        corner_colors = [(0, 0, 255), (255, 0, 0), (0, 255, 255), (255, 0, 255)]
-        for marker in int_corners:
-            marker = np.squeeze(marker)
-            markers.append(marker)
+            corner_colors = [(0, 0, 255), (255, 0, 0), (0, 255, 255), (255, 0, 255)]
+            for marker in int_corners:
+                marker = np.squeeze(marker)
+                markers.append(marker)
 
-            for xy_corner, corner_color in zip(marker, corner_colors):
-                cv2.circle(img, (xy_corner[0], xy_corner[1]), 8, corner_color, -1)
+                for xy_corner, corner_color in zip(marker, corner_colors):
+                    cv2.circle(img, (xy_corner[0], xy_corner[1]), 8, corner_color, -1)
 
-        marker_locations = []
-        marker_angles = []
-        # red, blue, yellow, purple (TL, TR, BR, BL) or (0, 1, 2, 3).
-        for marker in markers:
-            angles = np.array([
-                np.arctan2(marker[0][1] - marker[3][1], marker[0][0] - marker[3][0]),
-                np.arctan2(marker[1][1] - marker[2][1], marker[1][0] - marker[2][0])])
+            marker_locations = []
+            marker_angles = []
 
-                # Can use these two parallel lines also, but fails sometimes.
-                # np.fmod(np.arctan2(marker[0][1] - marker[1][1], marker[0][0] - marker[1][0]) - np.pi / 2, np.pi),
-                # np.fmod(np.arctan2(marker[3][1] - marker[2][1], marker[3][0] - marker[2][0]) - np.pi / 2, np.pi)])
+            for marker in markers:
+                angles = np.array([
+                    np.arctan2(marker[0][1] - marker[3][1], marker[0][0] - marker[3][0]),
+                    np.arctan2(marker[1][1] - marker[2][1], marker[1][0] - marker[2][0])])
 
-            angle = np.degrees(np.mean(angles))
-            marker_locations.append(np.mean(marker, axis=0).astype('int'))
-            marker_angles.append(angle)
+                angle = np.degrees(np.mean(angles))
+                angle = wrap_angle(angle)
+                marker_locations.append(np.mean(marker, axis=0).astype('int'))
+                marker_angles.append(angle)
 
+            for pos in marker_locations:
+                cv2.circle(img, (pos[0], pos[1]), 8, (0, 255, 0), -1)
 
-        for pos in marker_locations:
-            cv2.circle(img, (pos[0], pos[1]), 8, (0, 255, 0), -1)
+            if bot1.next == 'NONE':
+                bot1 = final_path(bot1, img)
 
-        cv2.imshow('frame',img)
-        out.write(img)
+            if current_position == get_position(bot1.target) and bot1.next == 'END':
+                bot1.complete = True
+                break
+
+            elif current_position == get_position(bot1.target):
+                if bot1.next == 'RIGHT':
+                    right()
+                elif bot1.next == 'LEFT':
+                    left()
+                elif bot1.next == 'BACK':
+                    back()
+                elif bot1.next == 'PAY':
+                    pay()
+                bot1.orientation = bot1.next_orientation
+                bot1.next = target(bot1)
+
+            bot1 = pid(bot1, position)
+
+            driver = webdriver.Chrome(executable_path='/home/vishwesh/Desktop/Geekbot/chromedriver')
+            driver.get("http://192.168.11.6/forward?r="+str(right_enable)+"&l="+str(left_enable))
+            driver.close()
+
+            cv2.imshow('frame',img)
+            out.write(img)
 
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-
-main()
